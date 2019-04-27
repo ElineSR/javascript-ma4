43f7546314ab541a82503952035ffadfe9e3451c
ElineSR <elinesr@hotmail.com>
2019-04-27 12:48:34 +0200

    nearly finished
    Lacking no 2

---
+++ level1/question2/script.js
@@ -0,0 +1,18 @@
+// fetch("https://jsonplaceholder.typicode.com/todos")
+// 
+//     .then(result => result.json())
+//   .then((res) => {
+//     createTodoCards(res);
+//   })
+// .catch(err => console.log(err))
+// 
+// 
+// function createTodoCards(result){
+//     var details = document.getElementById('personDetails');
+//   details.innerHTML += "<span>Name: " + result.name + "</span>";
+//   details.innerHTML += "<span>Height:   " + result.height + "CM</span>";
+//   details.innerHTML += "<span>Mass: " + result.mass + "KG</span>";
+//   details.innerHTML += "<span>Gender:   " + result.gender + "</span>";
+// }
+
+Unfinished
