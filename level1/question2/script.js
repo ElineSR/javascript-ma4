f53d61cc5117cd154b7eaeedd32925a33460c41e
Eline Selenius Riise <45076042+ElineSR@users.noreply.github.com>
2019-04-28 20:32:21 +0200

    Update script.js
    Still getting an undefined var, but the rest seems like it might work.

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,18 +1,41 @@
-// fetch("https://jsonplaceholder.typicode.com/todos")
-// 
-//     .then(result => result.json())
-//   .then((res) => {
-//     createTodoCards(res);
-//   })
-// .catch(err => console.log(err))
-// 
-// 
-// function createTodoCards(result){
-//     var details = document.getElementById('personDetails');
-//   details.innerHTML += "<span>Name: " + result.name + "</span>";
-//   details.innerHTML += "<span>Height:   " + result.height + "CM</span>";
-//   details.innerHTML += "<span>Mass: " + result.mass + "KG</span>";
-//   details.innerHTML += "<span>Gender:   " + result.gender + "</span>";
-// }
 
-Unfinished
+var requestURL = "https://jsonplaceholder.typicode.com/todos";
+var request = new XMLHttpRequest();
+request.open('GET',requestURL);
+
+request.responseType = 'text';
+request.send();
+
+request.onload = function(){
+    var cardsTodoText = request.response;
+    var cardsTodo = JSON.parse(cardsTodoText);
+    showCards (cardsTodo);
+};
+
+
+
+function showCards(jsonObj){
+    var cards = jsonObj[''];
+};
+
+for (var i = 0; i < cards.length; i++){
+    var myArticle = document.createElement('article');
+    var myH2 = document.createElement('h2');
+    var myPara1 = document.createElement('p');
+    var myPara2 = document.createElement('p');
+    var myPara3 = document.createElement('p');
+    
+    myH2.textContent = cards[i].userId;
+    myPara1.textContent = 'Id: ' + cards[i].id;
+    myPara2.textContent = 'Title: ' + cards[i].title;
+    myPara3.textContent = 'Completed: ' + cards[i].completed;
+    
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+ 
+    div.appendChild(myArticle);    
+    
+    
+}
