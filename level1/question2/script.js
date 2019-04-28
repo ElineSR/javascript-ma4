5b2644951678d47d0e759673b7c46fd8e7b60815
Eline Selenius Riise <45076042+ElineSR@users.noreply.github.com>
2019-04-28 21:29:24 +0200

    Update script.js
    tried another way, but the cards var is still undefined. Unsure of why.

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,41 +1,53 @@
+<script>
+//var requestURL = "https://jsonplaceholder.typicode.com/todos";
+//var request = new XMLHttpRequest();
+//request.open('GET',requestURL);
 
-var requestURL = "https://jsonplaceholder.typicode.com/todos";
-var request = new XMLHttpRequest();
-request.open('GET',requestURL);
+//request.responseType = 'text';
+//request.send();
 
-request.responseType = 'text';
-request.send();
-
-request.onload = function(){
-    var cardsTodoText = request.response;
-    var cardsTodo = JSON.parse(cardsTodoText);
-    showCards (cardsTodo);
-};
+//request.onload = function(){
+//	var cardsTodoText = request.response;
+//	var cardsTodo = JSON.parse(cardsTodoText);
+//	showCards (cardsTodo);
+//};
 
 
 
-function showCards(jsonObj){
-    var cards = jsonObj[''];
-};
-
-for (var i = 0; i < cards.length; i++){
-    var myArticle = document.createElement('article');
-    var myH2 = document.createElement('h2');
-    var myPara1 = document.createElement('p');
-    var myPara2 = document.createElement('p');
-    var myPara3 = document.createElement('p');
-    
-    myH2.textContent = cards[i].userId;
-    myPara1.textContent = 'Id: ' + cards[i].id;
-    myPara2.textContent = 'Title: ' + cards[i].title;
-    myPara3.textContent = 'Completed: ' + cards[i].completed;
-    
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
- 
-    div.appendChild(myArticle);    
-    
-    
+//function showCards(jsonObj){
+//	var cards = jsonObj[''];	
+//};
+var cardsTodo = new XMLHttpRequest();
+cardsTodo.onload = function(){
+	if(cardsTodo.status === 200){
+		var cards = JSON.parse(cardsTodo.responseText);
+	}
 }
+for (var i = 0; i <cards.length; i++){
+	var myArticle = document.createElement('article');
+	var myH2 = document.D('h2');
+	var myPara1 = document.createElement('p');
+	var myPara2 = document.createElement('p');
+	var myPara3 = document.createElement('p');
+	
+	myH2.textContent = cards[i].userId;
+	myPara1.textContent = 'Id: ' + cards[i].id;
+	myPara2.textContent = 'Title: ' + cards[i].title;
+	myPara3.textContent = 'Completed: ' + cards[i].completed;
+	
+	myArticle.appendChild(myH2);
+	myArticle.appendChild(myPara1);
+	myArticle.appendChild(myPara2);
+	myArticle.appendChild(myPara3);
+ 
+	div.appendChild(myArticle);	
+	
+};
+
+cardsTodo.open('GET', "https://jsonplaceholder.typicode.com/todos", true);
+cardsTodo.send(null);
+
+
+
+
+</script>
